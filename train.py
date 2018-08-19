d5fede6fa66740dc4bdc476eb45c27aca3eef093
Catastropha <tscorpan@yahoo.com>
2018-08-19 17:30:51 +0000

    added validation loss

--- train.py
+++ train.py
@@ -56,18 +56,27 @@ def main():
                 
         print()
         print('Testing accuracy on validation set...')
-        total = 0
         correct = 0
-        for ii, (inputs, labels) in enumerate(validloader):
-            inputs, labels = inputs.to(device), labels.to(device)
-            optimizer.zero_grad()
-            outputs = model.forward(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+        total = 0
+        running_loss = 0
+        every = 100
+        steps = 0
+        with torch.no_grad():
+            for data in validloader:
+                steps += 1
+                images = data[0].cuda(async=True)
+                labels = data[1].cuda(async=True)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                loss = criterion(outputs, labels)
+
+                running_loss += loss.item()
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+                if steps % every == 0 or steps % len(validloader) == 0:
+                    print("Validation Loss: {:.4f}".format(running_loss/every))
+                    running_loss = 0
                 
         print("Accuracy: {:.2f}%\n".format(100 * correct / total))
                 
